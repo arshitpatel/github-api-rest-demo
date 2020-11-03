23af489aa7e90e48bd32ce7308499bc3c1dd0cba
kbhimaji <arshit_kajavadara@optum.com>
2020-11-04 00:03:36 +0530

    intial commit

---
+++ src/main/java/com/informatica/demo/App.java
@@ -0,0 +1,34 @@
+package com.informatica.demo;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+	@Value("${http.port}")
+	private int httpPort;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+}
